15a69b7048557bc5bdeeacbff89b91cedb808e6a
Sam <arbikhan.007@gmail.com>
2020-08-21 17:51:45 +0530

    Completed Scroll Behaviour and Functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 import '../css/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import revealOnScroll from './modules/revealOnScroll'
 
 let mobileMenu = new MobileMenu();
+new revealOnScroll(document.querySelectorAll('.feature-item'), 75);
+new revealOnScroll(document.querySelectorAll('.testimonials'), 60);
 
 if (module.hot) {
 	module.hot.accept();
---
+++ app/assets/scripts/modules/revealOnScroll.js
@@ -0,0 +1,56 @@
+import throttle from 'lodash/throttle'
+import debounce from 'lodash/debounce'
+
+class revealOnScroll{
+	constructor(els, thresholdPercent){
+		this.thresholdPercent = thresholdPercent;
+		this.itemsToReveal = els;
+		this.browserHeight = window.innerHeight;
+		this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+
+		this.events();
+		this.hideInitially();
+	}
+
+	events(){
+		window.addEventListener("scroll", this.scrollThrottle);
+		window.addEventListener("resize", debounce(() => {
+			console.log("Debounce Ran.");
+			this.browserHeight = window.innerHeight;
+		}, 333));
+	}
+
+	calcCaller(){
+		console.log('Scroll Function Ran.')
+		this.itemsToReveal.forEach(el => {
+			if(el.isRevealed == false){
+				this.calculateIfScrolledTo(el);
+			}
+		})
+	}
+
+	calculateIfScrolledTo(el){
+		if(window.scrollY + this.browserHeight > el.offsetTop){
+			console.log("Calculations made.");
+			let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100;
+			if (scrollPercent < this.thresholdPercent) {
+				el.classList.add("reveal-item--is-visible");
+				el.isRevealed = true;
+				if(el.isLastItem){
+					window.removeEventListener('scroll', this.scrollThrottle);
+				}
+			}
+		}
+	}
+
+	hideInitially(){
+		this.itemsToReveal.forEach(el => {
+			el.classList.add("reveal-item");
+			el.isRevealed = false;
+		});
+		
+		this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
+	}
+}
+
+export default revealOnScroll;
