6fa0f40dfe40c1c2207b169437091e2cd663c4c7
Sam <arbikhan.007@gmail.com>
2020-08-23 20:49:01 +0530

    Completed Modal and its Functionalities

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,25 @@ import MobileMenu from './modules/MobileMenu'
 import revealOnScroll from './modules/revealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+new MobileMenu();
+new StickyHeader();
 new revealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new revealOnScroll(document.querySelectorAll('.testimonials'), 60);
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => {
+	el.addEventListener("click", (e) => {
+		e.preventDefault();
+		if(typeof modal == "undefined"){
+			import(/*webpackChunkName: "modal"*/'./modules/Modal').then(x => {
+				modal = new x.default();
+				setTimeout(() => {modal.enableModalVisibility()}, 20);
+			}).catch(() => {console.log("There is an error.")});
+		}else{
+			modal.enableModalVisibility();
+		}
+	})
+})
 
 if (module.hot) {
 	module.hot.accept();
